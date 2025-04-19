bd984439bd6838ecf16bdc3eae139b6b23371bbb
qhgill <qhgill2@gmail.com>
2025-04-19 16:39:46 -0700

    sidebar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,15 @@
 import Prompt from "../components/Prompt";
 import placeholderimg from "@/public/placeholder.png";
 import Option from "../utils/options";
-import Question from "../components/Question";
+import Sidebar from "../components/Sidebar";
 
 const option1: Option = {
   placeholder: "text",
 };
 const Home = () => {
   return (
-    <div className="w-screen max-h-screen flex items-center justify-center">
-      <Question />
+    <div className="w-screen relative h-screen max-h-screen flex items-center justify-center bg-radial from-gray-100 to-gray-400">
+      <Sidebar budget={1} sustainStatus={1} />
       <div className="flex items-center justify-center">
         <Prompt
           month={1}
--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,15 +1,52 @@
 import React from "react";
 import Image from "next/image";
 import placeholder from "../../public/placeholder.png";
+import Question from "../components/Question";
 
-const sidebar = () => {
+interface SidebarTypes {
+  budget: number;
+  sustainStatus: number;
+}
+
+const Sidebar = ({ budget, sustainStatus }: SidebarTypes) => {
   return (
-    <div className="flex flex-col h-screen max-h-screen bg-gray-500 w-[10vw]">
-      <div>
-        <Image src={placeholder} alt="placeholder" className="" />
+    <div className="flex flex-col items-start justify-between h-screen max-h-screen w-[20vw] mx-4 absolute left-0 inset-y-0">
+      <div className="flex flex-col gap-4">
+        <Image
+          src={placeholder}
+          alt="logo"
+          className="aspect-square object-fill size-40 mt-5"
+        />
+        <Question />
+        <p className="text-start text-3xl">
+          budget
+          <br />${budget}
+        </p>
+        <p className="text-start text-3xl">
+          sustain:
+          <br />
+          {sustainStatus}
+        </p>
+      </div>
+      <div className="flex flex-col  justify-start gap-y-5 py-10">
+        <Image
+          src={placeholder}
+          alt="placeholder"
+          className="aspect-square object-fill size-30 hover:border-4 hover:border-black rounded-full duration-100"
+        />
+        <Image
+          src={placeholder}
+          alt="placeholder"
+          className="aspect-square object-fill size-30 hover:border-4 hover:border-black rounded-full duration-100"
+        />
+        <Image
+          src={placeholder}
+          alt="placeholder"
+          className="aspect-square object-fill size-30 hover:border-4 hover:border-black rounded-full duration-100"
+        />
       </div>
     </div>
   );
 };
 
-export default sidebar;
+export default Sidebar;
