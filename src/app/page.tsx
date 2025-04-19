175b0f0f46203afc957328403a4713395bbd95c1
Kevin Loritsch <kloritsch58@gmail.com>
2025-04-19 14:53:40 -0700

    here you go

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,16 @@
+import Image from "next/image";
+import placeholder from "@/public/placeholder.png"
+
 const Home = () => {
-  return <>Hello World</>;
+  return <>
+    <Image 
+    src={placeholder}
+    width={500}
+    height={500}
+    alt="quin"
+    />
+  
+  </>;
 };
 
 export default Home;
