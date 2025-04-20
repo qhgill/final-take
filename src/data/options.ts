a67a59bff60fb6710734cd42860ca791f2a8958d
qhgill <qhgill2@gmail.com>
2025-04-19 17:16:30 -0700

    data folder

---
+++ src/data/options.ts
@@ -0,0 +1,13 @@
+import Option from "../utils/options";
+
+export const productionOptions: Option[] = [
+    {
+        placeholder: "placeholder"
+    }
+]
+
+export const marketingOptions: Option[] = [
+    {
+        placeholder: "placeholder"
+    }
+]
