02fa6e06cc1f151811d4a06981e3bc49a4a1f644
qhgill <qhgill2@gmail.com>
2025-04-19 23:09:02 -0700

    choice options pt1

--- src/components/Prompt.tsx
+++ src/components/Prompt.tsx
@@ -1,7 +1,10 @@
 import { Option } from "../utils/options";
 import AnimateText from "./AnimateText";
-import { Choice } from "../utils/options";
 import TicketSelect from "./choicestyles/TicketSelect";
+import TicketCarousel from "./choicestyles/TicketCarousel";
+import FrameCarousel from "./choicestyles/FrameCarousel";
+import ClapperCarousel from "./choicestyles/ClapperCarousel";
+import BillboardCarousel from "./choicestyles/BillboardCarousel";
 
 interface PromptTypes {
   options: Option;
@@ -9,34 +12,29 @@ interface PromptTypes {
   handleSwap: (price: number, sustain: number, profit: number) => void;
 }
 
-const dummyset: Choice[] = [
-  {
-    id: 1,
-    title: "title1",
-    description: "the best choice",
-    price: 1,
-    sustain: 1,
-    profit: 1,
-  },
-  {
-    id: 2,
-    title: "title2",
-    description: "also the best choice",
-    price: 1,
-    sustain: 1,
-    profit: 1,
-  },
-];
-
 const Prompt = ({ options, month, handleSwap }: PromptTypes) => {
   return (
     <div className="flex flex-col w-[70vw] max-h-[90vh] justify-center gap-y-10 items-center">
       <p className="text-4xl font-bold">MONTH: {month}</p>
       <p className="text-6xl font-extrabold">{options.title}</p>
       <AnimateText text={options.description} typeSpeed={30} />
-      <TicketSelect choices={dummyset} handleSwap={handleSwap} />
+      {options.selectionView === 1 && (
+        <TicketCarousel choices={options.answers} handleSwap={handleSwap} />
+      )}
+      {options.selectionView === 2 && (
+        <FrameCarousel choices={options.answers} handleSwap={handleSwap} />
+      )}
+      {options.selectionView === 3 && (
+        <ClapperCarousel choices={options.answers} handleSwap={handleSwap} />
+      )}
+      {options.selectionView === 4 && (
+        <BillboardCarousel choices={options.answers} handleSwap={handleSwap} />
+      )}
+      {options.selectionView === 5 && (
+        <TicketSelect choices={options.answers} handleSwap={handleSwap} />
+      )}
 
-      {options.selectionView != 1 && (
+      {options.selectionView >= 6 && (
         <button
           onClick={() => handleSwap}
           className="cursor-pointer border px-4 py-2 rounded-lg bg-gray-200 hover:bg-gray-300 transition"
