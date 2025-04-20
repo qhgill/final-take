190b904641e40fdf11e7fd0ea2fbf3fe04278106
arieen <arielleharyanto@gmail.com>
2025-04-20 12:01:31 -0700

    hopefully help

--- src/components/choicestyles/TicketSelect.tsx
+++ src/components/choicestyles/TicketSelect.tsx
@@ -15,14 +15,14 @@ const TicketSelect = ({ choices, handleSwap }: ChoiceTypes) => {
       {choices.map((slide: Choice) => (
         <div key={slide.id}>
           <div
-            className="relative text-lg sm:text-2xl font-bold text-white sm:pl-10 pl-3  pr-15 sm:pr-40 py-5 sm:py-12  hover:cursor-pointer hover:scale-105 duration-100 h-[15vh] sm:h-[50vh]"
+            className="text-pretty relative text-lg sm:text-2xl font-bold text-white sm:pl-10 pl-3  pr-15 sm:pr-40 py-5 sm:py-12  hover:cursor-pointer hover:scale-105 duration-100 h-[15vh] sm:h-[50vh]"
             onClick={() => handleSwap(slide.price, slide.sustain, slide.profit)}
           >
-            <p className="text-bold text-xl sm:text-5xl sm:pt-40">
+            <p className="pl-5 text-bold text-base sm:text-5xl sm:pt-40">
               {slide.title}
             </p>
-            <p className="text-sm sm:text-2xl ">{slide.description}</p>
-            <p className="text-sm ml-4 sm:ml-0 sm:text-2xl ">
+            <p className="pl-5 text-xs sm:text-2xl ">{slide.description}</p>
+            <p className="pl-5 text-xs ml-4 sm:ml-0 sm:text-2xl ">
               ${formatPrice(slide.price)}
             </p>
             <Image
