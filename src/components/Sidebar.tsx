1a82a34a7018d9765e12d5e3519c60044b28dbe5
qhgill <qhgill2@gmail.com>
2025-04-20 06:03:39 -0700

    import

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import Image from "next/image";
-import logo from "../../public/logo.png";
 import grayStar from "@/public/star/grayStar.png";
 import yellowStar from "@/public/star/yellowStar.png";
 import Question from "../components/Question";
