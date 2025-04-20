42dfb81f4635f19921bc019c001083b29eb6290b
arieen <arielleharyanto@gmail.com>
2025-04-19 20:45:48 -0700

    director + office thing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
+import Image from "next/image";
 
 import Prompt from "../components/Prompt";
 import placeholderimg from "@/public/placeholder.png";
@@ -9,6 +10,9 @@ import { Option, User } from "../utils/options";
 import Sidebar from "../components/Sidebar";
 import { marketingOptions } from "../data/options";
 import { productionOptions } from "../data/options";
+
+import directorChair from "@/public/directorChair.png";
+import office from "@/public/office.png";
 
 const usedOptions: Option[] = [];
 
@@ -37,27 +41,149 @@ const options: Option[] = [
     description: "i love cats too",
     placeholder: "purr",
   },
+  {
+    month: 3,
+    title: "third title",
+    icon: placeholderimg,
+    description: "i love cats",
+    placeholder: "meow",
+  },
+  {
+    month: 4,
+    title: "fourth title",
+    icon: placeholderimg,
+    description: "i love cats too",
+    placeholder: "purr",
+  },
+  {
+    month: 5,
+    title: "fifth title",
+    icon: placeholderimg,
+    description: "i love cats",
+    placeholder: "meow",
+  },
+  {
+    month: 6,
+    title: "sixth title",
+    icon: placeholderimg,
+    description: "i love cats too",
+    placeholder: "purr",
+  },
+  {
+    month: 7,
+    title: "seventh title",
+    icon: placeholderimg,
+    description: "i love cats",
+    placeholder: "meow",
+  },
+  {
+    month: 8,
+    title: "eighth title",
+    icon: placeholderimg,
+    description: "i love cats too",
+    placeholder: "purr",
+  },
+  {
+    month: 9,
+    title: "title",
+    icon: placeholderimg,
+    description: "i love cats",
+    placeholder: "meow",
+  },
+  {
+    month: 10,
+    title: "another title",
+    icon: placeholderimg,
+    description: "i love cats too",
+    placeholder: "purr",
+  },
+  {
+    month: 11,
+    title: "title",
+    icon: placeholderimg,
+    description: "i love cats",
+    placeholder: "meow",
+  },
+  {
+    month: 12,
+    title: "another title",
+    icon: placeholderimg,
+    description: "i love cats too",
+    placeholder: "purr",
+  },
 ];
+
+type FixedAssets = {
+  [month: number]: string;
+};
+
+const fixedAssets: FixedAssets = {
+  1: office.src,
+  2: office.src,
+  3: office.src,
+  4: directorChair.src,
+  5: directorChair.src,
+  6: directorChair.src,
+  7: directorChair.src,
+  8: directorChair.src,
+  9: office.src,
+  10: directorChair.src,
+  11: office.src,
+  12: directorChair.src,
+};
 
 const Home = () => {
   const [selectedOption, setSelectedOption] = useState<Option>(options[0]);
   const [visible, setVisible] = useState(true);
   const [promptKey, setPromptKey] = useState(0);
+  const [currentMonth, setCurrentMonth] = useState(1);
+  const [currentFixedAsset, setCurrentFixedAsset] = useState<string>(
+    fixedAssets[user.month] || "",
+  );
+
+  useEffect(() => {
+    setCurrentFixedAsset(fixedAssets[currentMonth] || "");
+  }, [currentMonth]);
 
   const handleSwap = () => {
     setVisible(false);
 
-    // this needs to be changed to a random + uses month so is scuffed
-    setSelectedOption((prev) =>
-      prev.month === options[0].month ? options[1] : options[0],
+    const nextOptionIndex = options.findIndex(
+      (option) => option.month === currentMonth,
     );
+    if (nextOptionIndex !== -1) {
+      setSelectedOption(options[nextOptionIndex]);
+    }
+
     setPromptKey((prev) => prev + 1);
     setVisible(true);
+    setCurrentMonth((prevMonth) => {
+      const nextMonth = prevMonth + 1;
+      return nextMonth > Object.keys(fixedAssets).length ? 1 : nextMonth;
+    });
   };
+  //   // this needs to be changed to a random + uses month so is scuffed
+  //   setSelectedOption((prev) =>
+  //     prev.month === options[0].month ? options[1] : options[0]
+  //   );
+  //   setPromptKey((prev) => prev + 1);
+  //   setVisible(true);
+  // };
 
   return (
     <div>
-      <div className="w-screen relative h-screen flex items-center justify-center bg-radial from-gray-100 to-gray-400">
+      <div className="overflow-x-hidden w-screen relative h-screen flex items-center justify-center bg-radial from-gray-100 to-gray-400">
+        {currentFixedAsset && (
+          <div className="absolute bottom-0 right-0 z-10">
+            <Image
+              src={currentFixedAsset}
+              alt="Fixed Asset"
+              width={300}
+              height={300}
+            />
+          </div>
+        )}
+
         <Sidebar budget={user.budget} sustainStatus={user.sustStat} />
         <div className="flex items-center justify-center">
           <AnimatePresence mode="wait">
