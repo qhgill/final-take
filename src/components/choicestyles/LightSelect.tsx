1003f33baca7dfb51466200ec36824ef5cdad495
qhgill <qhgill2@gmail.com>
2025-04-20 01:27:27 -0700

    lights

--- src/components/choicestyles/LightSelect.tsx
+++ src/components/choicestyles/LightSelect.tsx
@@ -10,29 +10,27 @@ interface ChoiceTypes {
 
 const LightSelect = ({ choices, handleSwap }: ChoiceTypes) => {
   return (
-    <div className="grid grid-cols-2 gap-16 h-[40vh]">
+    <div className="grid grid-cols-2 h-[40vh]">
       {choices.map((slide: Choice) => (
-        <div key={slide.id}>
+        <div
+          key={slide.id}
+          className="relative hover:cursor-pointer hover:scale-105 duration-100"
+        >
           <div
-            className={`relative text-2xl font-bold text-black px-30 py-30 hover:cursor-pointer hover:scale-105 duration-100 ${slide.id === 1 ? "text-left" : "text-right"}`}
+            className="text-2xl font-bold text-black px-20 py-30 w-full  text-center"
             onClick={() => handleSwap(slide.price, slide.sustain, slide.profit)}
           >
             <p className="text-bold text-5xl">{slide.title}</p>
             <p className="text-2xl ">{slide.description}</p>
             <p className="text-2xl ">${slide.price}</p>
           </div>
+          <Image
+            src={spotlightimg}
+            alt="ticket"
+            className={`absolute -inset-y-[20vh] w-full h-[75vh] -z-1 object-contain ${slide.id === 1 ? "left-[5vw] rotate-y-0" : "right-[5vw] rotate-y-180"}`}
+          />
         </div>
       ))}
-      <Image
-        src={spotlightimg}
-        alt="ticket"
-        className="absolute inset-y-20 -left-[10vw] -z-1 h-full w-full object-contain rotate-y-0"
-      />
-      <Image
-        src={spotlightimg}
-        alt="ticket"
-        className="absolute inset-y-20 -right-[10vw] -z-1 h-full w-full object-contain rotate-y-180"
-      />
     </div>
   );
 };
