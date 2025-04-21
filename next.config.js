468230134a4e61f68451c5aa6686639ac7374c8d
qhgill <qhgill2@gmail.com>
2025-04-21 01:20:59 -0700

    maybe fix deployment

--- next.config.js
+++ next.config.js
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  env: {
-    OPENROUTER_API_KEY: process.env.OPENROUTER_API_KEY,
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
